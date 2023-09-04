f7513a3270756eb638a1ef97735991c6ef54bc7e
zikri124 <faisalsaja73@gmail.com>
2023-09-05 01:57:30 +0700

    feat: add navbar component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="navbar bg-neutral text-neutral-content grid justify-items-stretch">
+            <div className="justify-self-center">
+                <a className="btn btn-ghost normal-case text-xl" href="/">My Todo</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
